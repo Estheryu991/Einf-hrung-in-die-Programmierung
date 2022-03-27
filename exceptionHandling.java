9d67f3a2765695a186b5443b5690730e85bb03f5
Esther Hoeun Yu <hoeuyu@student.ethz.ch>
2022-03-27 13:55:39 +0200

    Create exceptionHandling.java

---
+++ exceptionHandling.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Main {
+  public static void main (String[] args) {
+    Scanner scanner = new Scanner(System.in);
+    String[] categories = { "PCs", "Notebooks", "Tablets", "Phones", "Accessories"};
+    
+    try {
+      System.out.print(categories[choice]);
+    }
+    catch(Exception e) {
+      System.out.print("Wrong Option");
+    }
+  }
+}
