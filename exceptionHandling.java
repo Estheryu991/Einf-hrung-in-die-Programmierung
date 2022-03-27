17bd9b941efabead0f74623431dbf9f8dabce716
Esther Hoeun Yu <hoeuyu@student.ethz.ch>
2022-03-27 15:16:35 +0200

    Update exceptionHandling.java

--- exceptionHandling.java
+++ exceptionHandling.java
@@ -3,6 +3,7 @@ import java.util.Scanner;
 public class Main {
   public static void main (String[] args) {
     Scanner scanner = new Scanner(System.in);
+    int choice = scanner.nextInt();
     String[] categories = { "PCs", "Notebooks", "Tablets", "Phones", "Accessories"};
     
     try {
